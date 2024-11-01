953a07153a561d5ad70476c8c1072c68626bbe7e
Manu Vanneste <125195959+manulykebe@users.noreply.github.com>
2024-11-01 16:10:08 +0100

    setup

--- src/demo-async-monitor.ts
+++ src/demo-async-monitor.ts
@@ -1,11 +1,19 @@
-import * as monitor from 'async-monitor.js';
+import {Logger, Group, sleep} from 'async-monitor.js';
 
 export function demoAsyncMonitor() {
-	const watchGroup = new monitor.Group({repeat: 0});
-	watchGroup.addWatch(() => monitor.sleep(undefined, false));
-	watchGroup.addWatch(() => monitor.sleep(undefined, false));
-	watchGroup.addWatch(() => monitor.sleep(undefined, false));
-	watchGroup.addWatch(() => monitor.sleep(undefined, false));
-	monitor.logger.log(watchGroup.loggerTree, ['tree', `tree-${watchGroup.id}`]);
-	watchGroup.watchAll();
+	const watchGroup = new Group();
+	watchGroup.addWatch(() => sleep(5, false));
+	watchGroup.addWatch(() => sleep(undefined, false));
+	watchGroup.addWatch(() => sleep(undefined, false));
+	watchGroup.watchAll().then(() => {
+		watchGroup.logger.log('watchGroup done');
+	});
+
+	const watchGroup2 = new Group();
+	watchGroup2.addWatch(() => sleep(5, false));
+	watchGroup2.addWatch(() => sleep(undefined, false));
+	watchGroup2.addWatch(() => sleep(undefined, false));
+	watchGroup2.watchAll().then(() => {
+		watchGroup2.logger.log('watchGroup2 done');
+	});
 }
